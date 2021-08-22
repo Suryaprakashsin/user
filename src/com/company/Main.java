b0f37067cf770193ad136460cb6c75723c672778
suryaprakash <singhyash2207@gmail.comm>
2021-08-22 11:05:09 +0530

    Initial commit: user

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int user = 16;
+        if(user < 18){
+            System.out.println("User less than 18");
+        }
+
+    }
+}
